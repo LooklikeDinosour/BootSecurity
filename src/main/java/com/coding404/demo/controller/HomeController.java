d79c0a4f4267c89042805970919fd97f0368cf69
LooklikeDinosour <rccar2003@gmail.com>
2023-08-16 08:39:32 +0900

    jwt token add

--- src/main/java/com/coding404/demo/controller/HomeController.java
+++ src/main/java/com/coding404/demo/controller/HomeController.java
@@ -1,15 +1,55 @@
 package com.coding404.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.coding404.demo.command.UserVO;
+import com.coding404.demo.user.MyUserDetails;
+import com.coding404.demo.user.UserMapper;
 
 @Controller
 public class HomeController {
 
+	@Autowired 
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Autowired
+	private UserMapper userMapper;
+	
+	
 	//홈
 	@GetMapping("/hello")
-	public String hello() {
+	public String hello(Authentication auth) {
+		
+		
+		//1st - 컨트롤러 매개변수에 Authentication 객체를 선언합니다.
+//		if(auth != null) {
+//		System.out.println(auth);
+//		
+//		MyUserDetails details = (MyUserDetails)auth.getPrincipal();
+//		
+//		System.out.println(details.getUsername());
+//		System.out.println(details.getPassword());
+//		System.out.println(details.getRole());
+//		}
+		
+		//2nd - 시큐리티세션을 직접사용
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();		
+		
+		if( authentication.getPrincipal() instanceof MyUserDetails) {
+			MyUserDetails details = (MyUserDetails)authentication.getPrincipal();
+			System.out.println(details);
+		}
+		
 		return "hello";
 	}
 	
@@ -19,8 +59,32 @@ public class HomeController {
 		return "join";
 	}
 	
-	@GetMapping("/login")
-	public String login() {
+	
+	
+	//회원가입요청
+	@PostMapping("/joinForm")
+	public String joinForm(UserVO vo) {
+		
+		
+		//비밀번호 암호화
+		String pw = bCryptPasswordEncoder.encode(vo.getPassword());
+		vo.setPassword(pw);
+		
+		//서비스영역 생략
+		//회원가입작업
+		userMapper.join(vo);
+		
+		return "redirect:/login"; //가입이후 로그인페이지이동
+	}
+	
+	//커스터마이징 로그인페이지
+	@GetMapping("/login") //requestparam 필수 err을 얻을 수 없기때문에 진입못함
+	public String login(@RequestParam( value = "err", required = false) String err, Model model) {
+		
+		if(err != null) {
+			model.addAttribute("msg", "아이디 비밀번호를 확인하세요");
+		}
+		
 		return "login";
 	}
 	
@@ -38,4 +102,18 @@ public class HomeController {
 	public @ResponseBody String usermypage() {
 		return "REST API user 마이페이지";
 	}
+	
+	@GetMapping("/deny")
+	public @ResponseBody String deny() {
+		return "페이지에 접근권한이 없습니다";
+	}
+	
+	//어드민 권한이 있어야 접근이 가능
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@GetMapping("/test")
+	public @ResponseBody String test() {
+		return "여기는 preAuthorize로 처리";
+	}
+	
+	
 }
--- src/main/java/com/coding404/demo/security/config/SecurityConfig.java
+++ src/main/java/com/coding404/demo/security/config/SecurityConfig.java
@@ -1,16 +1,33 @@
 package com.coding404.demo.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.coding404.demo.user.MyUserDetailService;
 
 @Configuration //설정파일
 @EnableWebSecurity //이 설정파일을 시큐리티 필터에 추가(필수)
+@EnableGlobalMethodSecurity(prePostEnabled = true) // 어노테이션으로 권한을 지정할 수 있게 합니다.
 public class SecurityConfig {
 
+	//나를 기억해 사용할 UserDetailService
+	@Autowired
+	private MyUserDetailService myUserDetailService;
+	
+	
+	
+	//비밀번호 암호화객체
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Bean
 	public SecurityFilterChain securityFilter(HttpSecurity http) throws Exception {
@@ -38,9 +55,9 @@ public class SecurityConfig {
 //														  .antMatchers("/user/**").hasRole("USER") //user는 user 권한
 //														  .antMatchers("/admin/**").hasRole("ADMIN") //admin페이지는 admin 권한
 //														  .anyRequest().permitAll()); // 그외에는 모든 페이지는 접근이 가능하다.
-
+		//권한 앞에는 ROLE_ 가 자동으로 설정된다. 
 		http.authorizeHttpRequests (authorize -> authorize.antMatchers("/all").authenticated() //all 인증필요
-														  .antMatchers("/user/**").hasAnyRole("USER","ADMIN", "TESTER") // 셋중 1개의 권한을 가지면 됨
+														  .antMatchers("/user/**").hasAnyRole("USER", "ADMIN", "TESTER") // 셋중 1개의 권한을 가지면 됨
 														  .antMatchers("/admin/**").hasRole("ADMIN") //admin페이지는 admin 권한
 														  .anyRequest().permitAll()); // 그외에는 모든 페이지는 접근이 가능하다.
 
@@ -50,13 +67,32 @@ public class SecurityConfig {
 		//http.formLogin(Customizer.withDefaults()); //기본로그인페이지 사용
 		
 		//사용자가 제공하는 폼기반 로그인기능을 사용할 수 있습니다.
-		http.formLogin().loginPage("/login");
+		http.formLogin()
+			.loginPage("/login")//로그인 화면
+			.loginProcessingUrl("/loginForm") //로그인시도 요청경로 -> 스프링이 로그인시도를 낚아채서 userDetailService객체로 연결
+			.defaultSuccessUrl("/all") //로그인 성공시 페이지
+			.failureUrl("/login?err=true") //로그인 실패시 이동할 url
+			.and() //http. 초기선언과 같은상태
+			.exceptionHandling().accessDeniedPage("/deny") //권한이 없을 때 이동할 리다이렉트 경로
+			.and()
+			.logout().logoutUrl("/logout").logoutSuccessUrl("/hello"); ///default로그아웃경로 / logout아웃 주소를 직접 작성할 수 있고, 로그아웃 성공시 리다이렉트할 경로 지정 가능
 		
 		
-		
-		http.formLogin(Customizer.withDefaults()); //기본로그인 페이지사용
-		
+		//나를 기억해
+		http.rememberMe()
+			.key("coding404") //토큰(쿠키)를 만들 비밀 키 (필수)
+			.rememberMeParameter("remember-me") //화면에서 전달받는 checked name명입니다 (필수)
+			.tokenValiditySeconds(60)// 쿠키(토큰)의 유효시간 (필수)
+			.userDetailsService(myUserDetailService) //토큰이 있을 때 실행시킬 userDetailService객체 (필수)
+			.authenticationSuccessHandler(customRememberMe()); //나를 기억해가 동작할 때, 실행할 핸들러 객체를 넣습니다. 
 		
 		return http.build();
+	}
+	
+	//customRememberMe
+	@Bean
+	public CustomRememberMe customRememberMe() {
+		CustomRememberMe me = new CustomRememberMe("/all");//리멤버미 성공시 실행시킬 리다이렉트 주소
+		return me;
 	}
 }
